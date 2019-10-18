b889d36c307c04809015184b81a60cb3849d938c
remektra <renata.oliveira.brito@gmail.com>
2019-10-18 09:04:22 -0300

    função para criar a lista de programas

--- util.py
+++ util.py
@@ -3,17 +3,15 @@
 import os 
 import sys
 
-if __name__ == "__main__":
-    
+
+def create_programs_list():
+
     source = '.\\quickfix\\python_programs'
 
     for root, dirs, files in os.walk(source, topdown=False):
         #for directory in dirs:
         for f in files:
-            if '.py' not in f:
-                print('entro no not')
-                pass
-            else:  
+            if f[-3:] == '.py':
                 with open('programs.txt', 'a') as programs_file:
                     name = "{}\n".format(f)
                     programs_file.write(name)
