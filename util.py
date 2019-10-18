4d2e215325455f6b1816091958c288d8d0bf74e9
stevao-andrade <stevao.andrade@gmail.com>
2019-10-18 10:32:40 -0300

    improve util.py

--- util.py
+++ util.py
@@ -6,12 +6,15 @@ import sys
 
 def create_programs_list():
 
-    source = '.\\quickfix\\python_programs'
-
+    source = 'python_programs'
+    
     for root, dirs, files in os.walk(source, topdown=False):
-        #for directory in dirs:
         for f in files:
             if f[-3:] == '.py':
                 with open('programs.txt', 'a') as programs_file:
                     name = "{}\n".format(f)
                     programs_file.write(name)
+
+
+if __name__ == "__main__":
+    create_programs_list()
