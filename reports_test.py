d8d65cb1089a60240c567937f18aaa3a92984eaf
stevao-andrade <stevao.andrade@gmail.com>
2019-10-22 12:17:54 -0300

    first version of tests running  through scripts

--- reports_test.py
+++ reports_test.py
@@ -1,45 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import sys
-import os
+import json
+import pytest
+
 import subprocess
+
+
+#local
 from util import create_programs_list
+
+
+
+def execute_python_module(algo, *args):
+    """
+    This function imports a given module and execute it with a list of *args parametrs then returns the execution value
+    """
+
+    #import the algorithm
+    module_name = "python_programs.{}".format(algo) 
+    module = __import__(module_name)    
+    fx = getattr(module, algo) 
+
+    try:
+
+        #execute the algorithm with the *args and return the execution value
+        return getattr(fx, algo)(*args)
+    except:
+
+        #whenever happens a error, show some information about it
+        return sys.exc_info()
+
+
+def load_tests(data):
+    """
+        This function handles the generation of the test data
+    """    
+    # Load module which contains test data
+    test_data = open("json_testcases/{}.json".format(data), 'r')
+    
+    #for each test case in the test data
+    for line in test_data:
+        py_testcase = json.loads(line)
+        test_in, test_out = py_testcase #get input and output values
+
+        #create a generator to use in the test
+        yield test_in, test_out 
+
+
+
+
+def test_module(stringinput):
+    """
+        This function handles the test execution
+    """
+
+    generator = load_tests(stringinput)
+    for inn, expected in generator:
+        actual = execute_python_module(stringinput, inn)
+        assert actual == expected
 
 
 
 
 if __name__ == "__main__":
     
-    if len(sys.argv) == 1:
+    algo = "bitcount"
 
-        try:
-            with open("programs.txt", "r") as programs_list:
-
-                for program in programs_list:
-                    print(program)
-        except:
-            create_programs_list()
-
-   
-    elif len(sys.argv) == 2:
-
-        programa = sys.argv[1]
-        
-        if ".py" not in programa:
-            programa += ".py"
-
-        source_dir = ".\\quickfix\\python_programs\\"
-        programa = source_dir + programa 
-
-        comando_test = "pytest --cov . {}".format(programa)
-        comando_test = "python3 {}".format(programa)
-        print("Command: {}".format(comando_test))
-        process = subprocess.call(comando_test, shell=True)
-
-
-
-    
-    else:
-        print('W: Usage: python3 report_test.py program | python3 report_test.py ') 
+    comando_test = 'pytest -s  --stringinput="{}"'.format(algo)
+    process = subprocess.call(comando_test, shell=True)
