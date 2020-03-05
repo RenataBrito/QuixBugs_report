87a5a200ea1006003e3924f4b8b94de2ef8da4bb
stevao-andrade <stevao.andrade@gmail.com>
2020-03-05 15:28:37 -0300

    move test from graphs scripts to working folder

--- working/test_breadth_first_search.py
+++ working/test_breadth_first_search.py
@@ -1,12 +1,18 @@
-from node import Node
-from breadth_first_search import breadth_first_search
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import pytest
+
+from pytest_report import ROOT_DIR
+
+from python_programs.node import Node
+from python_programs.breadth_first_search import breadth_first_search
 
 
-"""
-Driver to test breadth first search
-"""
-def main():
-    # Case 1: Strongly connected graph
+
+def test_strongly_connected_graph():
+
     # Output: Path found!
     station1 = Node("Westminster")
     station2 = Node("Waterloo", None, [station1])
@@ -14,14 +20,18 @@ def main():
     station4 = Node("Canary Wharf",  None, [station2, station3])
     station5 = Node("London Bridge",  None, [station4, station3])
     station6 = Node("Tottenham Court Road",  None, [station5, station4])
-
+    
+    
     if breadth_first_search(station6, station1):
         print("Path found!", end=" ")
     else:
         print("Path not found!", end=" ")
     print()
 
-    # Case 2: Branching graph
+
+
+def test_branching_graph():
+
     # Output: Path found!
     nodef =  Node("F")
     nodee =  Node("E")
@@ -36,24 +46,50 @@ def main():
         print("Path not found!", end=" ")
     print()
 
-    # Case 3: Two unconnected nodes in graph
+
+
+def test_two_unconnected_nodes_in_graph():
+    
     # Output: Path not found
+
+    nodef =  Node("F")
+    nodee =  Node("E")
+    noded =  Node("D")
+    nodec =  Node("C", None, [nodef])
+    nodeb =  Node("B", None, [nodee])
+    nodea =  Node("A", None, [nodeb, nodec, noded])
+        
     if breadth_first_search(nodef, nodee):
         print("Path found!", end=" ")
     else:
         print("Path not found!", end=" ")
     print()
 
-    # Case 4: One node graph
+
+def test_one_node_graph():
+    
     # Output: Path found!
+
+    nodef =  Node("F")
+    nodee =  Node("E")
+    noded =  Node("D")
+    nodec =  Node("C", None, [nodef])
+    nodeb =  Node("B", None, [nodee])
+    nodea =  Node("A", None, [nodeb, nodec, noded])
+    
     if breadth_first_search(nodef, nodef):
         print("Path found!", end=" ")
     else:
         print("Path not found!", end=" ")
     print()
 
-    # Case 5: Graph with cycles
+
+
+def test_graph_with_cycles():
+
     # Output: Path found!
+
+    #setup
     node1 = Node("1")
     node2 = Node("2")
     node3 = Node("3")
@@ -63,11 +99,14 @@ def main():
 
     node2.successors = [node6]
 
+    #exercice
     if breadth_first_search(node6, node1):
         print("Path found!", end=" ")
     else:
         print("Path not found!", end=" ")
     print()
 
-if __name__ == "__main__":
-    main()
+    #verify
+    
+
+    #cleanup
