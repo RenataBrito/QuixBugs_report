8886ac271543681d1f0986207a235f423be3dd73
Stevão Andrade <stevao.andrade@gmail.com>
2020-08-28 09:50:09 -0300

    update test cases of test_node.py

--- python_tests/test_node.py
+++ python_tests/test_node.py
@@ -1,12 +1,31 @@
-from python_programs.node import Node
+from node import Node
 
-node1 = Node('1')
 
 def test_successor():
-    assert node1.successor == None
+    n     = Node(value = 2)
+    node1 = Node(value = 1, successor= n)
+        
+    node3 = node1.successor
+
+    assert n.value == node3.value
 
 def test_successors():
-    assert node1.successors == []
+
+    n     = Node(value = 2)
+    n2    = Node(value = 3)
+    node1 = Node(value = 1, successors = [n, n2])
+        
+    node3 = node1.successors[0]
+
+    assert len(node1.successors) == 2
 
 def test_predecessors():
-    assert node1.predecessors == []
+
+    n     = Node(value = 2)
+    n2    = Node(value = 3)
+    n3    = Node(value = 4)
+    node1 = Node(value = 1, predecessors = [n, n2, n3])
+        
+    node3 = node1.predecessors[0]
+
+    assert len(node1.predecessors) == 3
